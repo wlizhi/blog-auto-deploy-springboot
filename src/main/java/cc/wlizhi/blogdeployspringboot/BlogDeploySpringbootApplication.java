98e21874dfb504f2f5d6246b560a451de317d8d2
wlizhi <17521692290@163.com>
2020-11-17 20:49:41 +0800

    the first commit

--- src/main/java/cc/wlizhi/blogdeployspringboot/BlogDeploySpringbootApplication.java
+++ src/main/java/cc/wlizhi/blogdeployspringboot/BlogDeploySpringbootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class BlogDeploySpringbootApplication {
 
 	public static void main(String[] args) {
+		// xx
 		SpringApplication.run(BlogDeploySpringbootApplication.class, args);
 	}
 
