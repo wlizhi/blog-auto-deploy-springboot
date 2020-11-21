382bf83fff712ceeded592a1f5429980d639a544
wlizhi <2456767505@qq.com>
2020-11-22 00:13:46 +0800

    cc

--- add-commit-push.sh
+++ add-commit-push.sh
@@ -2,5 +2,7 @@ git add .
 git commit -m cc
 git push origin master
 if test 'github' = $1; then
+echo 'push to origin-github master...'
 git push origin-github master
+echo 'push to origin-github master successful.'
 fi
